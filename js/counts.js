be6974ef2f81170adc58f4c740b1ed000d8bba51
Ben Godfrey <BenJetson@users.noreply.github.com>
2021-05-23 22:58:47 -0400

    Create some helper methods for counts. #116

--- js/counts.js
+++ js/counts.js
@@ -1,3 +1,52 @@
+const countDigits = (n) => {
+  if (isNaN(n) || !Number.isInteger(n)) throw "Digit count requires integer.";
+  else if (n < 0) throw "Cannot find digits for negative value.";
+  else if (n === 0) return 1;
+
+  let counter = 0;
+  let v = n;
+
+  while (v > 0) {
+    v = Math.floor(v / 10);
+    counter++;
+  }
+
+  return counter;
+};
+
+const findDigitNodes = (count) => {
+  // Will give a NodeList
+  const counter = document.getElementById("counter");
+  const found = counter.querySelectorAll("span.digit");
+
+  // Move nodes into an array for greater mutability.
+  let nodes = [];
+  for (const n of found) nodes.push(n);
+
+  const diff = nodes.length - count;
+  if (diff > 0) {
+    // We have more nodes than we need. Delete some in the amount of diff.
+    for (null; diff > 0; diff--) {
+      counter.remove(nodes.pop());
+    }
+  } else if (diff < 0) {
+    // We don't have enough nodes; make some more in the amount of diff.
+    for (null; diff < 0; diff++) {
+      const digitNode = document.createElement("span");
+      digitNode.classList.add("digit");
+      digitNode.innerText = "."; // placeholder text
+
+      // Hide digits from screen readers since their readout would be confusing.
+      digitNode.setAttribute("aria-hidden", true);
+
+      counter.prepend(digitNode);
+      nodes.unshift(digitNode);
+    }
+  }
+
+  return nodes;
+};
+
 window.counter.update = async () => {
   const counter = document.getElementById("counter"),
     key = window.counter.key,
