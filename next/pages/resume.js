d7b724696760f65232bbc6451fcffa732941e457
Ben Godfrey <BenJetson@users.noreply.github.com>
2022-09-05 18:55:40 -0400

    Display resume in JSON! #152

--- next/pages/resume.js
+++ next/pages/resume.js
@@ -1,7 +1,12 @@
+import { resumeData } from "../lib/resume";
+
 const Resume = () => (
   <>
     <h1>Resumé</h1>
-    <p>Some text here.</p>
+    <p>Well, the human-readable format isn't ready yet.</p>
+    <p>Here's the version for robots:</p>
+    <pre>{JSON.stringify(resumeData, null, 2)}</pre>
+    <p>You made it to the end. I guess it was easy to read then!</p>
   </>
 );
 
