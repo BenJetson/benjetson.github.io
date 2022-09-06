ab378f6713431d0fa23717187a1301a3721df2c2
Ben Godfrey <BenJetson@users.noreply.github.com>
2022-09-05 23:51:41 -0400

    Escape the quote as well. #152

--- next/pages/resume.js
+++ next/pages/resume.js
@@ -2,8 +2,9 @@ import { resumeData } from "../lib/resume";
 
 const Resume = () => (
   <>
-    <h1>{"Resumé"}</h1>
-    <p>Well, the human-readable format isn't ready yet.</p>
+    {/* These are in expressions since they contain HTML entities. */}
+    <h1>{`Resumé`}</h1>
+    <p>{`Well, the human-readable format isn't ready yet.`}</p>
     <p>Here's the version for robots:</p>
     <pre>{JSON.stringify(resumeData, null, 2)}</pre>
     <p>You made it to the end. I guess it was easy to read then!</p>
