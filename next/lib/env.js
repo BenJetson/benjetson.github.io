0326186175371f52aa98adb7e09efc75c51d9481
Ben Godfrey <BenJetson@users.noreply.github.com>
2022-09-06 00:08:35 -0400

    Do not use dynamic env variable lookups, they are not supported by Next.js. #152

--- next/lib/env.js
+++ next/lib/env.js
@@ -1,24 +1,23 @@
-const lookupNextEnv = (name) => process.env["NEXT_PUBLIC_" + name];
-
 /**
  * The TIER indicates Environment that the app is deployed and running on.
  * Can be one of `production`, `preview`, or `development`.
  */
-export const TIER = lookupNextEnv("VERCEL_ENV") ?? "development";
+export const TIER = process.env.NEXT_PUBLIC_VERCEL_ENV ?? "development";
 
 /**
  * The GIT_HASH indicates the SHA hash of the most recent commit in Git, which
  * this build was created upon.
  */
-export const GIT_HASH = lookupNextEnv("VERCEL_GIT_COMMIT_SHA") ?? "unknown";
+export const GIT_HASH =
+  process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA ?? "unknown";
 
 /**
  * The RECAPTCHA_SITE_KEY is a public key that is provided to the ReCAPTCHA
  * web service when making verification requests.
  */
-export const RECAPTCHA_SITE_KEY = lookupNextEnv("RECAPTCHA_SITE_KEY");
+export const RECAPTCHA_SITE_KEY = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY;
 
 /**
  * The CONTACT_FORM_ID is the FormSpree form identifier for the contact page.
  */
-export const CONTACT_FORM_ID = lookupNextEnv("CONTACT_FORM_ID");
+export const CONTACT_FORM_ID = process.env.NEXT_PUBLIC_CONTACT_FORM_ID;
