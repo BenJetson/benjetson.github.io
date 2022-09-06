331d19f2e1f06b1845a01f9a36b4e6676eed0ba0
Ben Godfrey <BenJetson@users.noreply.github.com>
2022-09-05 23:38:24 -0400

    Show git/tier info in the footer. #152

--- next/components/footer.js
+++ next/components/footer.js
@@ -1,9 +1,13 @@
+import { GIT_HASH, TIER } from "../lib/env";
 import { site } from "../lib/meta";
 
 const Footer = () => (
   <footer>
     <h2>{site.title}</h2>
     <p>{site.tagline}</p>
+    <pre>
+      build {GIT_HASH}-{TIER}
+    </pre>
   </footer>
 );
 
