0fdd034558339a44eff4c4a976b2ede410087c71
Ben Godfrey <BenJetson@users.noreply.github.com>
2022-09-14 19:03:55 -0400

    Rewrite footer and navbar with basic Chakra functionality. #152

--- next/components/footer.js
+++ next/components/footer.js
@@ -1,11 +1,17 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
 import { GIT_HASH } from "../lib/env";
 import { site } from "../lib/meta";
+import ContentContainer from "./content-container";
 
 const Footer = () => (
   <footer>
-    <h2>{site.title}</h2>
-    <p>{site.tagline}</p>
-    <pre>build {GIT_HASH}</pre>
+    <Box maxW="full" p={5} bgColor="gray.700" color="white">
+      <ContentContainer>
+        <Heading as="h2">{site.title}</Heading>
+        <Text>{site.tagline}</Text>
+        <pre>build {GIT_HASH}</pre>
+      </ContentContainer>
+    </Box>
   </footer>
 );
 
--- next/components/navbar.js
+++ next/components/navbar.js
@@ -1,23 +1,56 @@
 import { navItems, site } from "../lib/meta";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { Box, Button, Container, Heading, HStack } from "@chakra-ui/react";
+import ContentContainer from "./content-container";
+
+const NavLink = ({ href, name }) => {
+  const router = useRouter();
+  const isActive = router.asPath === href;
+
+  return (
+    <Link href={href}>
+      <Button
+        as="a"
+        colorScheme="whiteAlpha"
+        color="white"
+        // _hover={{
+        //   bg: "blue.100",
+        //   color: "blue.500",
+        // }}
+        // variant={isActive ? "outline" : "ghost"}
+        variant="ghost"
+        textDecoration={isActive ? "underline" : "none"}
+        textDecorationStyle="dotted"
+        textUnderlineOffset={5}
+      >
+        {name}
+      </Button>
+    </Link>
+  );
+};
 
 const Navbar = () => (
-  <nav>
-    <h1>
-      <Link href={"/"}>
-        <a>{site.title}</a>
-      </Link>
-    </h1>
-    <ul>
-      {navItems.map((item) => (
-        <li key={item.href}>
-          <Link href={item.href}>
-            <a>{item.name}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </nav>
+  <Box bgColor="blue.500" color="white" py={3} px={6}>
+    <ContentContainer>
+      <HStack>
+        <Link href="/">
+          <a>
+            <Heading as="h1">Ben's Realm</Heading>
+          </a>
+        </Link>
+        <Box flexGrow={1}></Box>
+
+        <HStack as="ul" listStyleType="none">
+          {navItems.map((item) => (
+            <li key={item.href}>
+              <NavLink {...item} />
+            </li>
+          ))}
+        </HStack>
+      </HStack>
+    </ContentContainer>
+  </Box>
 );
 
 export default Navbar;
