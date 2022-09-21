4367c2f6643f6554d716fb4e9b5394ba8a25ad2c
Ben Godfrey <BenJetson@users.noreply.github.com>
2022-09-21 17:45:57 -0400

    Pass page properties to the layout. #166

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -6,7 +6,7 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <Layout>
+      <Layout pageProps={pageProps}>
         <Component {...pageProps} />
       </Layout>
     </ChakraProvider>
