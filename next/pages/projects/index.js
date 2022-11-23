f0304924781cc498a89397e12f2be41c226615ba
Ben Godfrey <BenJetson@users.noreply.github.com>
2022-11-22 20:04:05 -0500

    First draft of projects page layout. #175

--- next/pages/projects/index.js
+++ next/pages/projects/index.js
@@ -1,39 +1,94 @@
 import { getAllProjectMetadata } from "../../lib/projects";
 import Link from "next/link";
-import { Heading, List, ListItem } from "@chakra-ui/react";
 import {
+  Button,
+  ButtonGroup,
   Card,
-  CardContent,
+  CardBody,
+  CardFooter,
   CardHeader,
-  CardTitle,
-} from "../../components/card";
+  Divider,
+  Heading,
+  Hide,
+  Image,
+  List,
+  ListItem,
+  Show,
+  SimpleGrid,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { CardTitle } from "../../components/card";
+import { formatAsMonthDate } from "../../lib/date";
 
 const Projects = ({ projects }) => {
   return (
     <>
       <Heading as="h2">Projects</Heading>
-      <List mt={3}>
+      <SimpleGrid columns={[1, 2, null, 3]} spacing={10}>
         {projects.map((project) => (
-          <ListItem key={project.filePath}>
-            <Link href={project.href}>
-              <Card>
-                <CardHeader>
+          // FIXME this shold use LinkOverlay
+          // https://chakra-ui.com/docs/components/link-overlay
+          <Link href={project.href} key={project.filePath}>
+            <Card
+              // backgroundColor={"#f8f9fa"}
+              // direction={{ base: "column", sm: "row" }}
+              alignItems="stretch"
+              overflow="hidden"
+            >
+              <CardBody flexShrink={1} flexGrow={0} mb={0}>
+                <Image
+                  borderRadius="lg"
+                  objectFit="cover"
+                  // maxW={{ base: "100%", sm: "180px" }}
+                  // minW={{ base: "100%", sm: "180px" }}
+                  // minH={{ base: "100%", sm: "180px" }}
+                  // maxH={{ base: "180px", sm: "100%" }}
+                  maxW="100%"
+                  minW="100%"
+                  minH="100%"
+                  maxH="180px"
+                  src={project.frontMatter.photo}
+                  alt={project.frontMatter["photo-alt"]}
+                />
+              </CardBody>
+              {/* <Show below="sm">
+                <Divider />
+              </Show>
+              <Hide below="sm">
+                <Divider orientation="vertical" height="inherit" />
+              </Hide> */}
+              <Stack>
+                {/* <CardHeader>
+                    <Heading level={3} size="md">
+                      {project.frontMatter.title}
+                    </Heading>
+                  </CardHeader> */}
+                <CardBody>
                   <CardTitle level={3}>{project.frontMatter.title}</CardTitle>
-                </CardHeader>
-                <CardContent>
-                  Posted on {project.frontMatter.date} <br />
-                  Preview:{" "}
-                  {
-                    // FIXME excerpts not working!
-                    project.excerpt || "no excerpt available"
-                  }
-                </CardContent>
-              </Card>
-            </Link>
-            <br />
-          </ListItem>
+                  <Text textStyle="subtitle">
+                    {formatAsMonthDate(project.frontMatter.date)}
+                  </Text>
+                  <Text>
+                    {
+                      // FIXME excerpts not working!
+                      project.excerpt ||
+                        "No excerpt is available for this project."
+                    }
+                  </Text>
+                </CardBody>
+                {/* <CardFooter>
+                  <ButtonGroup>
+                    <Button>Read</Button>
+                    <Button variant="ghost">Repository</Button>
+                    <Button variant="ghost">Webpage</Button>
+                  </ButtonGroup>
+                </CardFooter> */}
+              </Stack>
+            </Card>
+          </Link>
         ))}
-      </List>
+      </SimpleGrid>
     </>
   );
 };
