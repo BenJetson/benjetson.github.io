f98e2e44cff67ff75b9a7d11114fa672f08a8e07
Ben Godfrey <BenJetson@users.noreply.github.com>
2022-11-25 16:22:19 -0500

    Revert card.js to 39fe127.

--- next/components/card.js
+++ next/components/card.js
@@ -1,4 +1,11 @@
+import { Flex } from "@chakra-ui/react";
 import { Heading } from "./typography";
+
+const cardSpacing = 4;
+const cardBorderWidth = 1;
+const cardBorderColor = "gray.100";
+const cardBorderStyle = "solid";
+const cardBorderRadius = 5;
 
 /**
  *
@@ -10,8 +17,40 @@ import { Heading } from "./typography";
  */
 export const CardTitle = ({ children, ...props }) => {
   return (
-    <Heading my={0} size="md" {...props}>
+    <Heading my={0} {...props}>
       {children}
     </Heading>
   );
 };
+
+export const CardHeader = ({ children }) => (
+  <Flex
+    borderBottomColor={cardBorderColor}
+    borderBottomWidth={cardBorderWidth}
+    borderBottomStyle={cardBorderStyle}
+    shrink={0}
+    p={cardSpacing}
+  >
+    {children}
+  </Flex>
+);
+
+export const CardContent = ({ children }) => (
+  <Flex p={cardSpacing}>{children}</Flex>
+);
+
+export const Card = ({ children }) => (
+  <Flex
+    direction="column"
+    bg="white"
+    // alignItems="center"
+    borderColor={cardBorderColor}
+    borderWidth={cardBorderWidth}
+    borderStyle={cardBorderStyle}
+    borderRadius={cardBorderRadius}
+    boxShadow="sm"
+    // mt={cardSpacing}
+  >
+    {children}
+  </Flex>
+);
