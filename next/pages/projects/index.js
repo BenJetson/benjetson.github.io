83d51e418665a7f674cc60101814cc5e794eab9e
Ben Godfrey <BenJetson@users.noreply.github.com>
2023-01-16 16:16:47 -0500

    Add bottom section with link to GitHub and waves to project page. #175

--- next/pages/projects/index.js
+++ next/pages/projects/index.js
@@ -2,22 +2,37 @@ import { Box, Heading, Link, Text } from "@chakra-ui/react";
 import NextLink from "next/link";
 import ContentContainer from "../../components/content-container";
 import { ProjectCollection } from "../../components/projects";
+import { Wave, waveVariants } from "../../components/waves";
+import { socials } from "../../lib/meta";
 import { getAllProjectMetadata } from "../../lib/projects";
 
+/**
+ * @typedef { import('@chakra-ui/react').BackgroundProps['bgColor'] } ColorToken
+ */
+
+const PageSection = ({ children, ...props }) => (
+  <Box as="section" pt={4} pb={10} {...props}>
+    <ContentContainer>{children}</ContentContainer>
+  </Box>
+);
+
+/**
+ * @param {Object} param0 the props object.
+ * @param {string} param0.label the label for the section.
+ * @param {ColorToken} param0.bgColor the background color for the section.
+ */
 const ProjectSection = ({
   label,
   projects,
   columns = undefined,
-  backgroundColor = undefined,
+  bgColor = undefined,
 }) => (
-  <Box as="section" backgroundColor={backgroundColor} pt={4} pb={10}>
-    <ContentContainer>
-      <Heading as="h3" size="lg">
-        {label}
-      </Heading>
-      <ProjectCollection projects={projects} columns={columns} />
-    </ContentContainer>
-  </Box>
+  <PageSection bgColor={bgColor}>
+    <Heading as="h3" size="lg">
+      {label}
+    </Heading>
+    <ProjectCollection projects={projects} columns={columns} />
+  </PageSection>
 );
 
 const Projects = ({ otherProjects, featuredProjects }) => {
@@ -32,6 +47,7 @@ const Projects = ({ otherProjects, featuredProjects }) => {
         <Text>
           This is a collection of some of my most notable works. If you like my
           work, perhaps I might be able to{" "}
+          {/* FIXME links are not underlined or different color?! */}
           <NextLink
             passHref
             href="/contact"
@@ -43,13 +59,35 @@ const Projects = ({ otherProjects, featuredProjects }) => {
           .
         </Text>
       </ContentContainer>
+      <Wave
+        variant={waveVariants.SEMI1}
+        bgColor={"blue.50"}
+        fgColor={"white"}
+      />
       <ProjectSection
         label="Featured Projects"
         projects={featuredProjects}
-        backgroundColor="blue.50"
+        bgColor="blue.50"
         columns={[1, 2]}
       />
+      <Wave
+        variant={waveVariants.SEMI1}
+        invert
+        fgColor={"blue.50"}
+        bgColor={"white"}
+      />
       <ProjectSection label="Other Projects" projects={otherProjects} />
+
+      <PageSection>
+        <Heading as="h3" size="lg">
+          Missing Something?
+        </Heading>
+        <Text>
+          This site only showcases select projects from my portfolio. You can
+          find even more projects on my{" "}
+          <Link href={socials.github.url}>GitHub profile</Link>.
+        </Text>
+      </PageSection>
     </>
   );
 };
